b5808a415f856f7d093f1f2f8d1cc785f4dd02ba
Cosmin Adrian <cosminadrian218@gmail.com>
2022-05-11 16:19:53 +0300

    Add main components

---
+++ src/components/BookPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookItem from './BookItem';
+import Form from './Form';
+
+function BookPage() {
+  return (
+    <div>
+      <BookItem />
+      <Form />
+    </div>
+  );
+}
+
+export default BookPage;
