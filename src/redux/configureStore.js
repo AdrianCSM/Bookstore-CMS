7db00a5fcc18a6213813bbd1d1c8422acbac6909
Cosmin Adrian <cosminadrian218@gmail.com>
2022-05-14 05:07:39 +0300

    Correct code, create action to get books from API, fetch using AXIOS

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,17 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categories from './categories/categories';
 
 const reducer = combineReducers({
   booksReducer,
-  categories,
+  categories
 });
 
 const store = createStore(
   reducer,
-  applyMiddleware(logger),
+  applyMiddleware(thunk, logger),
 );
 
 export default store;
